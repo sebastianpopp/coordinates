b8270a86aad78f66e227f3086c5d1928ca297926
Sebastian Popp <git@sebastianpopp.com>
2025-07-07 15:40:44 +0200

    remove deafult locations

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,24 +1,13 @@
 import Alpine from 'alpinejs';
 import CoordinatesParser from './CoordinatesParser';
-import Coordinates from './Coordinates';
 
 Alpine.data('coordinates', () => ({
     query: '',
     coordinates: null,
 
     init() {
-        const coordinates = [
-            new Coordinates(53.56643, 10.01484), // Hamburg
-            new Coordinates(34.05598, -118.32655), // Los Angeles
-            new Coordinates(-33.85758, 151.17868), // Sydney
-            new Coordinates(-22.89435, -43.39272), // Rio de Janeiro
-        ];
-
-        this.query = coordinates[Math.floor(Math.random() * coordinates.length)].toDecimalMinutes();
-
         this.$nextTick(() => {
             this.$refs.query.focus();
-            this.$refs.query.setSelectionRange(0, this.$refs.query.value.length);
         });
 
         Alpine.effect(() => {
@@ -28,7 +17,7 @@ Alpine.data('coordinates', () => ({
                 ? coordinatesParser.getCoordinates()
                 : null;
 
-            if (this.query !== '' && !coordinates.some(c => c.toDecimalMinutes() === this.query)) {
+            if (this.query !== '') {
                 window._paq = window._paq || [];
                 _paq.push(['trackSiteSearch',
                     this.query,
